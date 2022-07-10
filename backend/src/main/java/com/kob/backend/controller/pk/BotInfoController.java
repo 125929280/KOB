6ef8ce4bdd50b0546cee6d276f2ea3df911aaa7f
zhaozheng <125929280@qq.com>
2022-07-10 21:26:50 +0800

    项目骨架搭建好了

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -3,12 +3,18 @@ package com.kob.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/pk")
 public class BotInfoController {
 
     @RequestMapping("/getBotInfo")
-    public String getBotInfo() {
-        return "hhhh";
+    public Map<String, String> getBotInfo() {
+        Map<String, String> bot = new HashMap<>();
+        bot.put("name", "tiger");
+        bot.put("rating", "1500");
+        return bot;
     }
 }
