45a97fb194062d3b178b48032f5f8b9f69ac8826
zhaozheng <125929280@qq.com>
2022-08-08 21:37:46 +0800

    配置Mysql和注册登录模块

--- backend/src/test/java/com/kob/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/kob/backend/BackendApplicationTests.java
@@ -2,12 +2,17 @@ package com.kob.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class BackendApplicationTests {
 
     @Test
     void contextLoads() {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        System.out.println(passwordEncoder.encode("19990728"));
+        System.out.println(passwordEncoder.matches("19990728", "$2a$10$c2RbPcw2LkHovza/lyoWBOvoIumMgXnsvTaEydRhjtCWIXHy0kds."));
     }
 
 }
