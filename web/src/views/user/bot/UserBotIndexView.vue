1356d6f4836d11c062020fceac63f4a90ccffd0c
zhaozheng <125929280@qq.com>
2022-08-12 14:11:31 +0800

    实现Bot的增删改查逻辑

--- web/src/views/user/bot/UserBotIndexView.vue
+++ web/src/views/user/bot/UserBotIndexView.vue
@@ -25,7 +25,7 @@
               <div class="modal-dialog modal-xl">
                 <div class="modal-content">
                   <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">创建Bot</h5>
+                    <h5 class="modal-title">创建Bot</h5>
                     <button
                       type="button"
                       class="btn-close"
@@ -108,10 +108,97 @@
                       type="button"
                       class="btn btn-secondary"
                       style="margin-right: 10px"
+                      data-bs-toggle="modal"
+                      :data-bs-target="'#update-update-modal-' + bot.id"
                     >
                       修改
                     </button>
-                    <button type="button" class="btn btn-danger">删除</button>
+                    <!-- Modal -->
+                    <div
+                      class="modal fade"
+                      :id="'update-update-modal-' + bot.id"
+                      tabindex="-1"
+                    >
+                      <div class="modal-dialog modal-xl">
+                        <div class="modal-content">
+                          <div class="modal-header">
+                            <h5 class="modal-title">创建Bot</h5>
+                            <button
+                              type="button"
+                              class="btn-close"
+                              data-bs-dismiss="modal"
+                              aria-label="Close"
+                            ></button>
+                          </div>
+                          <div class="modal-body">
+                            <div class="mb-3">
+                              <label for="add-bot-title" class="form-label"
+                                >名称</label
+                              >
+                              <input
+                                v-model="bot.title"
+                                type="text"
+                                class="form-control"
+                                id="add-bot-title"
+                                placeholder="请输入Bot名称"
+                              />
+                            </div>
+                            <div class="mb-3">
+                              <label
+                                for="add-bot-description"
+                                class="form-label"
+                                >简介</label
+                              >
+                              <textarea
+                                v-model="bot.description"
+                                class="form-control"
+                                id="add-bot-description"
+                                rows="3"
+                                placeholder="请输入Bot简介"
+                              ></textarea>
+                            </div>
+                            <div class="mb-3">
+                              <label for="add-bot-content" class="form-label"
+                                >代码</label
+                              >
+                              <textarea
+                                v-model="bot.content"
+                                class="form-control"
+                                id="add-bot-content"
+                                rows="7"
+                                placeholder="请编写Bot代码"
+                              ></textarea>
+                            </div>
+                          </div>
+                          <div class="modal-footer">
+                            <div class="error-message">
+                              {{ bot.error_message }}
+                            </div>
+                            <button
+                              type="button"
+                              class="btn btn-primary"
+                              @click="update_bot(bot)"
+                            >
+                              保存修改
+                            </button>
+                            <button
+                              type="button"
+                              class="btn btn-secondary"
+                              data-bs-dismiss="modal"
+                            >
+                              取消
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <button
+                      type="button"
+                      class="btn btn-danger"
+                      @click="remove_bot(bot)"
+                    >
+                      删除
+                    </button>
                   </td>
                 </tr>
               </tbody>
@@ -181,11 +268,57 @@ export default {
       });
     };
 
+    const remove_bot = (bot) => {
+      $.ajax({
+        url: "http://127.0.0.1:3000/user/bot/remove/",
+        type: "post",
+        data: {
+          bot_id: bot.id,
+        },
+        headers: {
+          Authorization: "Bearer " + store.state.user.token,
+        },
+        success(resp) {
+          console.log(resp);
+          if (resp.error_message === "success") {
+            refresh_bots();
+          }
+        },
+      });
+    };
+
+    const update_bot = (bot) => {
+      bot_add.error_message = "";
+      $.ajax({
+        url: "http://127.0.0.1:3000/user/bot/update/",
+        type: "post",
+        data: {
+          bot_id: bot.id,
+          title: bot.title,
+          description: bot.description,
+          content: bot.content,
+        },
+        headers: {
+          Authorization: "Bearer " + store.state.user.token,
+        },
+        success(resp) {
+          if (resp.error_message === "success") {
+            Modal.getInstance("#update-update-modal-" + bot.id).hide();
+            refresh_bots();
+          } else {
+            bot_add.error_message = resp.error_message;
+          }
+        },
+      });
+    };
+
     refresh_bots();
     return {
       bots,
       bot_add,
       add_bot,
+      remove_bot,
+      update_bot,
     };
   },
 };
